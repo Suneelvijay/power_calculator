4d201477c769f9e3489c235ad12d62c782650f0a
Suneelvijay M <136710677+Suneelvijay@users.noreply.github.com>
2023-11-29 20:57:56 +0530

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
-def calculate_power(voltage, current, resistance, conductance, energy, time, power_factor):
+def calculate_power(voltage, current, resistance, conductance, energy, time):
     power = None
-    
+
     if voltage and current:
         power = voltage * current
 
-    elif current and resistance:
-        power = (current ** 2) * resistance
-
     elif voltage and resistance:
         power = (voltage ** 2) / resistance
+
+    elif current and resistance:
+        power = (current ** 2) * resistance
 
     elif current and conductance:
         power = (current ** 2) / conductance
@@ -18,25 +18,31 @@ def calculate_power(voltage, current, resistance, conductance, energy, time, pow
     elif energy and time:
         power = energy / time
 
-    elif voltage and current and power_factor:
-        power = voltage * current * power_factor
-
     return power
 
 
 def main():
     st.title('Power Calculator')
 
-    voltage = st.number_input('Enter voltage:')
-    current = st.number_input('Enter current:')
-    resistance = st.number_input('Enter resistance:')
-    conductance = st.number_input('Enter conductance:')
-    energy = st.number_input('Enter energy:')
-    time = st.number_input('Enter time:')
-    power_factor = st.number_input('Enter power factor:')
+    selected_param_1 = st.selectbox('Select first parameter', ('V', 'I', 'R', 'G', 'E'))
+    selected_param_2 = st.selectbox('Select second parameter', ('I', 'R', 'V', 'G', 't'))
+
+    input_values = {
+        'V': st.number_input('Enter voltage:'),
+        'I': st.number_input('Enter current:'),
+        'R': st.number_input('Enter resistance:'),
+        'G': st.number_input('Enter conductance:'),
+        'E': st.number_input('Enter energy:'),
+        't': st.number_input('Enter time:')
+    }
 
     if st.button('Calculate Power'):
-        result = calculate_power(voltage, current, resistance, conductance, energy, time, power_factor)
+        result = calculate_power(input_values.get(selected_param_1),
+                                 input_values.get(selected_param_2),
+                                 input_values.get('R'),
+                                 input_values.get('G'),
+                                 input_values.get('E'),
+                                 input_values.get('t'))
         if result is not None:
             st.write(f"Calculated Power: {result} watts")
         else:
